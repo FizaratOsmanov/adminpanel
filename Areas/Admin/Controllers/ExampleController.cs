f1cc6fc8bd97e8bfe61324ba964e3f104fe99f3f
FizaratOsmanov <fizaratzp-ab205@code.edu.az>
2025-01-19 01:54:51 +0400

    //

---
+++ Areas/Admin/Controllers/ExampleController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+namespace AdminPanel.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class ExampleController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        public IActionResult Update()
+        {
+            return View();
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,19 @@
+namespace AdminPanel
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+            builder.Services.AddControllersWithViews();
+
+            var app = builder.Build();
+            app.UseStaticFiles();
+            app.MapControllerRoute(
+            name: "areas",
+            pattern: "{area:exists}/{controller=Dashboard}/{action=Index}/{id?}");
+
+            app.Run();
+        }
+    }
+}
